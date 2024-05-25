2d6205bd8427ab5f860e4873e82d01cc41f21391
tetuyoko <tyokoyama53@gmail.com>
2024-05-25 17:58:12 +0900

    update zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,5 @@
-# Envs.
+## Env
 #
-#
-
 ## Ruby
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/local/mysql/bin
@@ -12,51 +10,27 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 ## Go
 export GOPATH=$HOME/.go
-export GOROOT=
 export PATH=$PATH:$GOPATH/bin
-export MGNCV=/Users/tetuyoko/gocode/src/mgncvtr
-export GOPRIVATE=github.com/zeals-co-ltd/protobuf
+
+## dotnet
+export PATH="$PATH:$HOME/.dotnet/tools"
 
 ## Python
 export PATH="/usr/local/opt/python@3.12/bin:$PATH"
-#export PATH="$HOME/.pyenv/shims:${PATH}"
-#export PYENV_ROOT="$HOME/.pyenv"
-#export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$HOME/.poetry/bin:$PATH"
 
-## Deno
-export DENO_INSTALL="$HOME/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
-
-## Tensorflow
+### Tensorflow
 export CUDA_HOME=/usr/local/cuda
 export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$CUDA_HOME/lib"
 export PATH="$CUDA_HOME/bin:$PATH"
 
 ## JavaScript
+### yarn
 export PATH="$HOME/.yarn/bin:$PATH"
 
-# Cocos2d-x
-export COCOS_CONSOLE_ROOT=/Users/yokoyama_tetsuro/cocos2d-x-3.2/tools/cocos2d-console/bin
-export PATH=$COCOS_CONSOLE_ROOT:$PATH
-export ANT_ROOT=/usr/local/Cellar/ant/1.9.3/libexec/bin
-export PATH=$ANT_ROOT:$PATH
-export PATH=$PATH:/usr/sbin/:/usr/local/bin:/usr/bin
-
-## Android
-export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export ANDROID_NDK_ROOT=/opt/android-ndk-r8e
-export V_TARGET_ID="android-16"
-export IPHONE_SIM=~/Library/Application\ Support/iPhone\ Simulator
-
-## React-Native
-export ANDROID_HOME=${HOME}/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
-
-export NDK_ROOT=${ANDROID_NDK_ROOT} 
-export NDK_CCACHE=`which ccache`
-export ANDROID_SDK_ROOT=${ANDROID_HOME}
+### Deno
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
 
 ## GCP
 export GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json
@@ -74,10 +48,9 @@ export PATH="/usr/local/opt/mysql@8.0/bin:$PATH"
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
 
-export PATH="$HOME/.poetry/bin:$PATH"
+## k8s
 export PATH="$PATH:$HOME/.kube/plugins/jordanwilson230"
 
-export PATH="$PATH:$HOME/.dotnet/tools"
 
 export DEPLOY_KEY=$(cat ~/.ssh/github/id_rsa | base64)
 export TERM=xterm-256color
